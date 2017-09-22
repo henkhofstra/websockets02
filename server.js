855273dcadc4d69543497c2757cd599a94f3978a
henkhofstra <henk@can-west.nl>
2017-09-22 10:31:49 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ app.use(express.static('./'));
 require("./controller/controller.js")(app,io);
 
 http.listen(8080,function(){
-    console.log("Node Server is setup and it is listening on http://"+ip.address()+":8080");
+    console.log("Websocket Server is opgestart en luistert op http://"+ip.address()+":8080");
 })
